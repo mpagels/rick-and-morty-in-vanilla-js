2c923e70ea8b7fc6dcff565d99459478d127f179
Martin <office@martinpagels.de>
2021-07-10 01:16:14 +0200

    Load characters depending on filter (select option)

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import createCard from "./createCard";
 
 const mainElement = document.querySelector("main");
-
+const selectElement = document.querySelector("select");
 const loadButton = document.querySelector(".btn-load-characters");
+
 loadButton.addEventListener("click", () => {
+  const url = createUrlUsingSelectFilter();
   mainElement.innerText = "";
-  fetch("https://rickandmortyapi.com/api/character")
+
+  fetch(url)
     .then((response) => response.json())
     .then((data) =>
       data.results.forEach((character) => {
@@ -15,3 +18,16 @@ loadButton.addEventListener("click", () => {
     )
     .catch((error) => console.log("Something went wrong." + error));
 });
+
+function createUrlUsingSelectFilter() {
+  switch (selectElement.value) {
+    case "dead":
+      return "https://rickandmortyapi.com/api/character/?status=dead";
+    case "alive":
+      return "https://rickandmortyapi.com/api/character/?status=alive";
+    case "unknown":
+      return "https://rickandmortyapi.com/api/character/?status=unknown";
+    default:
+      return "https://rickandmortyapi.com/api/character";
+  }
+}
