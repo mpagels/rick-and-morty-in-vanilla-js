858f732d3ea8f2650c14c5bc5ecbf9bfa16077b5
Martin <office@martinpagels.de>
2021-07-09 23:56:24 +0200

    Add fetch() function
    - fetch https://rickandmortyapi.com/api/character
    - and console.log the data.result array

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import createCard from "./createCard";
 
+fetch("https://rickandmortyapi.com/api/character")
+  .then((response) => response.json())
+  .then((data) => console.log(data.results))
+  .catch((error) => console.log("Something went wrong." + error));
+
 const mainElement = document.querySelector("main");
 
 const card = createCard();
