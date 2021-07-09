82f0026fde93f10bd7b6283f00aaa40980a1e0fe
Martin <office@martinpagels.de>
2021-07-10 01:03:11 +0200

    When click button, character will load from api

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import createCard from "./createCard";
+
 const mainElement = document.querySelector("main");
 
-fetch("https://rickandmortyapi.com/api/character")
-  .then((response) => response.json())
-  .then((data) =>
-    data.results.forEach((character) => {
-      const characterCard = createCard(character);
-      mainElement.append(characterCard);
-    })
-  )
-  .catch((error) => console.log("Something went wrong." + error));
+const loadButton = document.querySelector(".btn-load-characters");
+loadButton.addEventListener("click", () => {
+  fetch("https://rickandmortyapi.com/api/character")
+    .then((response) => response.json())
+    .then((data) =>
+      data.results.forEach((character) => {
+        const characterCard = createCard(character);
+        mainElement.append(characterCard);
+      })
+    )
+    .catch((error) => console.log("Something went wrong." + error));
+});
